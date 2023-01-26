d22e0b8337556b94c3b2d31ddd7112bd70dfbbcd
edu-lauer <lauer.edu@gmail.com>
2023-01-26 15:11:34 -0300

    projeto dnc-coffee

---
+++ projeto-dnc-coffee/main.js
@@ -0,0 +1,9 @@
+function showButton(id) {
+    let btn = document.getElementById(id)
+    btn.style.visibility = 'visible'
+}
+
+function hideButton(id) {
+    let btn = document.getElementById(id)
+    btn.style.visibility = 'hidden'
+}
